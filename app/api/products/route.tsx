97d8d5690555021b62ea379164366287685a3a70
w.szutryk <w.szutryk@optimumpareto.com>
2024-05-05 14:19:30 +0200

    rebuild api

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,13 +1,72 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import clientPromise from "../../../lib/mongodb";
+import {
+  DB_NAME,
+  PRODUCTS_COLLECTION_NAME,
+  PRODUCTS_EMBEDDING_PATH,
+  PRODUCTS_INDEX_NAME,
+} from "@/shared/consts/db.const";
+import { SEARCH_QUERY_PARAM } from "@/shared/consts/search.const";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
+    const query = request.nextUrl.searchParams.get(SEARCH_QUERY_PARAM);
+
     const client = await clientPromise;
-    const db = client.db("next-mongo-vector-search");
-    const movies = await db.collection("products").find({}).limit(10).toArray();
-    return new NextResponse(JSON.stringify(movies));
+
+    if (!query) {
+      const db = client.db("next-mongo-vector-search");
+      const movies = await db
+        .collection("products")
+        .find({})
+        .limit(10)
+        .toArray();
+      return new NextResponse(JSON.stringify(movies));
+    }
+
+    // Call OpenAI API to get the embeddings.
+    const url = "https://api.openai.com/v1/embeddings";
+    const openai_key = process.env.Open_ai;
+    let embedding = null;
+
+    let response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${openai_key}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        input: query,
+        model: "text-embedding-ada-002",
+      }),
+    });
+    const respData = await response.json();
+
+    if (response.status === 200) {
+      embedding = respData.data[0].embedding;
+    } else {
+      throw new Error(
+        `Failed to get embedding. Status code: ${response.status}`
+      );
+    }
+
+    const db = client.db(DB_NAME);
+    const products = await db
+      .collection(PRODUCTS_COLLECTION_NAME)
+      .aggregate([
+        {
+          $vectorSearch: {
+            index: PRODUCTS_INDEX_NAME,
+            path: PRODUCTS_EMBEDDING_PATH,
+            queryVector: embedding,
+            numCandidates: 100,
+            limit: 10,
+          },
+        },
+      ])
+      .toArray();
+    return new NextResponse(JSON.stringify(products));
   } catch (e) {
     console.error(e);
   }
@@ -24,8 +83,7 @@ export async function POST(req: Request) {
   try {
     const [data, client] = await Promise.all([req.formData(), clientPromise]);
     const dataObj = Object.fromEntries(data);
-    console.log("data", dataObj);
-    const db = client.db("next-mongo-vector-search");
+    const db = client.db(DB_NAME);
 
     const validationResult = ProductSchema.safeParse(dataObj);
 
@@ -33,7 +91,9 @@ export async function POST(req: Request) {
       throw new Error(validationResult.error.errors[0].message);
     }
 
-    const product = await db.collection("products").insertOne(dataObj);
+    const product = await db
+      .collection(PRODUCTS_COLLECTION_NAME)
+      .insertOne(dataObj);
 
     return new NextResponse(
       JSON.stringify({ id: product.insertedId.toString() })
