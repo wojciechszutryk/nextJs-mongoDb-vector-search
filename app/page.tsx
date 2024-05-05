802d32695347da7dc5a2f93e9d31eae34d9d0c69
w.szutryk <w.szutryk@optimumpareto.com>
2024-05-05 14:18:25 +0200

    rebuild homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,38 @@
-import clientPromise from "../lib/mongodb";
-import { Product } from "../types/product.interface";
+import { Product } from "../shared/types/product.interface";
+import ProductList from "./components/ProductList";
+import { SEARCH_QUERY_PARAM } from "@/shared/consts/search.const";
 
 interface Props {
   products: Product[];
 }
 
-const Products: React.FC<Props> = async () => {
-  const products = await getData();
+const Products: React.FC<Props> = () => {
+  const getProducts = async (phrase: string): Promise<Product[]> => {
+    "use server";
+    try {
+      const queryParams = new URLSearchParams();
+      queryParams.append(SEARCH_QUERY_PARAM, phrase);
+
+      const url = new URL(`${process.env.APP_URL}/api/products`);
+      url.search = queryParams.toString();
+
+      const response = await fetch(url).then((res) => res.json());
+
+      return response;
+    } catch (e) {
+      console.error(e);
+      return [];
+    }
+  };
+
   return (
-    <div>
-      <h1>Top 10 Products of All Time</h1>
-      <p>
-        <small>(According to me)</small>
-      </p>
-      <ul>
-        {products.map((product) => (
-          <li key={product._id}>
-            <h2>{product.product_name}</h2>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <main>
+      <h1 className="text-4xl font-bold text-center mb-8">
+        Mongo.db + Next.js Semantic Vector Search Demo
+      </h1>
+      <ProductList search={getProducts} />
+    </main>
   );
 };
 
 export default Products;
-
-export const getData = async (): Promise<Product[]> => {
-  try {
-    const client = await clientPromise;
-    const db = client.db("next-mongo-vector-search");
-    const products = await db
-      .collection("products")
-      .find({})
-      .limit(10)
-      .toArray();
-
-    return JSON.parse(JSON.stringify(products));
-  } catch (e) {
-    console.error(e);
-    return [];
-  }
-};
