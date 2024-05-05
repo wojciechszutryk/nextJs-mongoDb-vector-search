dede2d56866d920da3751aadbd88c3fc64d3d9b8
w.szutryk <w.szutryk@optimumpareto.com>
2024-05-05 14:18:53 +0200

    add styles to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} m-auto mt-10 max-w-screen-lg`}>
+        {children}
+      </body>
     </html>
   );
 }
